93551eee5a1c49fe156952fdf158e6aefffa3038
Mineru <68109760+mineruking@users.noreply.github.com>
2024-05-30 01:28:10 +0900

    더미 메인 페이지 추가

--- lib/myhome.dart
+++ lib/myhome.dart
@@ -44,13 +44,25 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           automaticallyImplyLeading: false,
           backgroundColor: Color.fromARGB(255, 255, 255, 255),
-          title: Text(
-            'BOGGLE',
-            style: TextStyle(color: Color.fromARGB(196, 147, 159, 248)),
+          title: Row(
+            children: [
+              Text(
+                'BOGGLE',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 147, 159, 248),
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                '.',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 196, 42, 250),
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
           ),
           centerTitle: false,
           bottom: TabBar(
-            tabAlignment: TabAlignment.start, // 탭을 왼쪽으로 정렬
+            tabAlignment: TabAlignment.start,
             isScrollable: true, // 탭이 스크롤 가능하게 설정
             tabs: [
               Tab(text: '어항'),
@@ -63,8 +75,314 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: TabBarView(
           children: [
-            Center(child: Text('어항 페이지')),
-            Center(child: Text('포인트 페이지')),
+            SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    color: Colors.blue,
+                    height: 200,
+                    child: Center(
+                      child: Text(
+                        '어항 이미지',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '이달의 수질',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        DropdownButton<String>(
+                          value: '북대동',
+                          items: <String>['북대동', '남대동'].map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          onChanged: (_) {},
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '1.1 ppm',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: List.generate(10, (index) {
+                            return Expanded(
+                              child: Container(
+                                height: 8,
+                                color: index < 2
+                                    ? Colors.blue
+                                    : (index < 3
+                                        ? Colors.lightBlue
+                                        : (index < 5
+                                            ? Colors.green
+                                            : (index < 8
+                                                ? Colors.orange
+                                                : Colors.red))),
+                              ),
+                            );
+                          }),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('1'),
+                            Text('2'),
+                            Text('3'),
+                            Text('5'),
+                            Text('8'),
+                            Text('10<'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '수질 등급',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Icon(Icons.water, color: Colors.blue),
+                            SizedBox(width: 8),
+                            Text('매우 좋음',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue)),
+                            SizedBox(width: 8),
+                            Text('간단한 정수 후 마실 수 있음'),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Icon(Icons.water, color: Colors.lightBlue),
+                            SizedBox(width: 8),
+                            Text('좋음',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.lightBlue)),
+                            SizedBox(width: 8),
+                            Text('일반 정수 처리 후 마실 수 있음'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            PageView(
+              children: [
+                SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '홍길동 님의 포인트',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          padding: EdgeInsets.all(16.0),
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 248, 248, 248),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '655 P',
+                                style: TextStyle(
+                                    fontSize: 48,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color.fromARGB(255, 196, 42, 250)),
+                              ),
+                              SizedBox(height: 10),
+                              ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      Color.fromARGB(255, 8, 8, 8), // 배경 색상
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                ),
+                                child: Text(
+                                  '뽑기 바로 진행하러 가기',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          '포인트 리포트',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  '1,136 P',
+                                  style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey),
+                                ),
+                                SizedBox(height: 10),
+                                Container(
+                                  width: 50,
+                                  height: 100,
+                                  color: Colors.grey,
+                                ),
+                                SizedBox(height: 10),
+                                Text('전체 사용자 평균 포인트'),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  '655 P',
+                                  style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromARGB(255, 196, 42, 250)),
+                                ),
+                                SizedBox(height: 10),
+                                Container(
+                                  width: 50,
+                                  height: 60,
+                                  color: Color.fromARGB(255, 196, 42, 250),
+                                ),
+                                SizedBox(height: 10),
+                                Text('나의 포인트'),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          '나의 순위',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 10),
+                        ListTile(
+                          leading: Icon(Icons.equalizer, color: Colors.grey),
+                          title: Text('나는 상위 몇 프로?'),
+                          subtitle: Text('나의 포인트 순위를 알려드립니다.'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '나의 순위',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 20),
+                        Center(
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                width: 200,
+                                height: 200,
+                                child: Stack(
+                                  children: [
+                                    Center(
+                                      child: Container(
+                                        width: 200,
+                                        height: 200,
+                                        child: CircularProgressIndicator(
+                                          value: 0.7,
+                                          strokeWidth: 20,
+                                          color:
+                                              Color.fromARGB(255, 196, 42, 250),
+                                          backgroundColor: Colors.grey[200],
+                                        ),
+                                      ),
+                                    ),
+                                    Center(
+                                      child: Text(
+                                        '상위 70%',
+                                        style: TextStyle(
+                                            fontSize: 24,
+                                            fontWeight: FontWeight.bold,
+                                            color: Color.fromARGB(
+                                                255, 196, 42, 250)),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.emoji_events,
+                                      color: Colors.amber, size: 32),
+                                  SizedBox(width: 10),
+                                  Text(
+                                    '홍길동 님은 6950등 입니다.',
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
         bottomNavigationBar: BottomNavigationBar(
@@ -82,7 +400,7 @@ class _MyHomePageState extends State<MyHomePage> {
             BottomNavigationBarItem(
                 label: '실천', icon: Icon(Icons.check_circle)),
             BottomNavigationBarItem(label: '커뮤니티', icon: Icon(Icons.group)),
-            BottomNavigationBarItem(label: 'MY', icon: Icon(Icons.person))
+            BottomNavigationBarItem(label: 'MY', icon: Icon(Icons.person)),
           ],
         ),
       ),
