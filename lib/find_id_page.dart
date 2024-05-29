01d70e7a04a8335af5001211f4ac56d7f2516d6b
Yoon Byul <yb97pe@gmail.com>
2024-05-29 09:53:12 +0900

    find id page UI 구현

--- lib/find_id_page.dart
+++ lib/find_id_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:boggle/login_page.dart';
 
 class FindIDPage extends StatefulWidget {
   const FindIDPage({super.key});
@@ -8,6 +9,8 @@ class FindIDPage extends StatefulWidget {
 }
 
 class _FindIDPageState extends State<FindIDPage> {
+  final TextEditingController _emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +18,42 @@ class _FindIDPageState extends State<FindIDPage> {
           title: const Text("ID 찾기"),
           centerTitle: true,
         ),
-        body: Container());
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+              width: 300,
+              child: TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  filled: true,
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                  contentPadding: EdgeInsets.all(8),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: 300,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromRGBO(199, 166, 233, 1),
+                  foregroundColor: Colors.white,
+                ),
+                  onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context)=>const LoginPage()),
+                              );
+                          },
+                  //페이지 연결 확인을 위한 임시 코드 
+                //onPressed: _login,
+                child: const Text('ID 찾기'),
+              ),
+            ),
+          ],),
+        ));
   }
 }
