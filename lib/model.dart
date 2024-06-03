e5003942659712c0ed377b95ca8e465f3d351ee9
Kyongmin Yea <kminnyes@gmail.com>
2024-06-03 10:17:34 +0900

    세제인증 모델 형성

---
+++ lib/model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Certification{
+  final String cleansername;// 세제 이름
+  final String certificationcheck; // 세제 인증 여부
+  final String certificationdate; // 인증 날짜
+
+  Certification(this.cleansername, this.certificationcheck, this.certificationdate);
+}
+
